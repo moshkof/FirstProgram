451f6869080b6dca1ae3a10e8f80306d06a54dc6
cataclysm78 <cataclysm78@yandex.ru>
2021-12-23 17:54:39 +0300

    Добавление pyinsaller

--- sampleForm.py
+++ sampleForm.py
@@ -2,7 +2,6 @@ from PyQt5 import QtWidgets, uic
 import sys
 
 app = QtWidgets.QApplication(sys.argv)
-
-window = uic.loadUi("template.ui")
+window = uic.loadUi("D:\projects\python\LearnPaythonAI\FirstProgram\\template.ui")
 window.show()
 sys.exit(app.exec())
